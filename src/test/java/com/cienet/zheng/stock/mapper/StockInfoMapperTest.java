b83eb8777c25d9ea2e7cd5a809ce60946faff60c
zhengshan <zhengshan1@xiaomi.com>
2022-11-08 16:10:44 +0800

    test some gson

--- src/test/java/com/cienet/zheng/stock/mapper/StockInfoMapperTest.java
+++ src/test/java/com/cienet/zheng/stock/mapper/StockInfoMapperTest.java
@@ -1,7 +1,14 @@
 package com.cienet.zheng.stock.mapper;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import com.google.common.base.Predicate;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,7 +26,7 @@ import com.cienet.zheng.stock.dao.UserOperate;
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
-@MybatisTest
+//@MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class StockInfoMapperTest {
 
@@ -53,6 +60,66 @@ class StockInfoMapperTest {
     void findBySecurityCode() {
         List<StockInfo> res = stockInfoMapper.findBySecurityCode("INF");
         assertEquals(res.size(), 2);
+    }
+
+
+    String json = "{" +
+            "  \"head\":{" +
+            "      \"mid\":2475710519," +
+            "      \"golds\":0," +
+            "      \"availableGolds\":0," +
+            "      \"frozenGolds\":0," +
+            "      \"exchangeRate\":100" +
+            "  }" +
+            "}";
+    @Test
+    void findByTradeID() {
+        JsonParser parser = new JsonParser();
+        JsonElement appKey = parser.parse(json);
+        JsonElement element = optimizeElement(appKey);
+        System.out.println(element);
+    }
+
+    public JsonElement optimizeElement(JsonElement jsonElement) {
+        GsonOptimizer gsonOptimizer = clientTaskOptimizer();
+        if (jsonElement == null) {
+            return null;
+        }
+        if (jsonElement.isJsonNull()) {
+            return jsonElement;
+        }
+
+        if (jsonElement.isJsonPrimitive()) {
+            if (gsonOptimizer.valueFilter.apply(jsonElement.getAsJsonPrimitive())) {
+                return null;
+            }
+            return jsonElement;
+        } else if (jsonElement.isJsonObject()) {
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+            if (jsonObject.entrySet().isEmpty()) {
+                return jsonObject;
+            }
+            return gsonOptimizer.optimizeObject(jsonObject);
+        } else { // jsonElement.isJsonArray()
+            JsonArray jsonArray = jsonElement.getAsJsonArray();
+            if (jsonArray.size() == 0) {
+                return jsonArray;
+            }
+            return gsonOptimizer.optimizeArray(jsonArray);
+        }
+    }
+
+    private static GsonOptimizer clientTaskOptimizer() {
+        GsonOptimizer.Builder builder = new GsonOptimizer.Builder();
+        builder.skipField("sTime").skipField("eTime").skipField("totalNum").skipField("visible").skipField("golds")
+                .removeField("serverExtension")
+                .doubleFilter(new Predicate<Double>() {
+                    @Override
+                    public boolean apply(Double input) {
+                        return input == 0.0d || input == -1.0d;
+                    }
+                });
+        return builder.build();
     }
 
     @Test
